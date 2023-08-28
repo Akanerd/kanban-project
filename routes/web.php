15ca8a53c29666da54bed9832a452a82394bc842
Azzasafah <muhammad.hafizh2016@gmail.com>
2023-08-28 23:35:21 +0700

    Develop task update

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,8 @@ Route::prefix('tasks')
         Route::get('/','index')->name('index');
         Route::get('/create','create')->name('create');
         Route::post('/','store')->name('store');
-        Route::get('{id}/edit','edit')->name('edit');
+        Route::get('/{id}', 'show')->name('show'); 
+        Route::get('/{id}/edit','edit')->name('edit');
+        Route::put('/{id}','update')->name('update');
+        Route::delete('/{id}','destroy')->name('destroy');
     });
